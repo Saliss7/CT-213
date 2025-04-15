158d9d50eee7537fb745b646f875d37a278504e2
MatheusFelipe <matheusfeliperb.mf@gmail.com>
2025-04-14 21:26:04 -0300

    Lab 4: implementation of PSO

--- Laboratório 4 - Otimização com Métodos Baseados em População/particle_swarm_optimization.py
+++ Laboratório 4 - Otimização com Métodos Baseados em População/particle_swarm_optimization.py
@@ -16,8 +16,10 @@ class Particle:
         :param upper_bound: upper bound of the particle position.
         :type upper_bound: numpy array.
         """
-        # Todo: implement
-        pass  # Remove this line
+        self.best_position = np.zeros(np.size(lower_bound))
+        self.best_value = -inf
+        self.x = np.random.uniform(lower_bound, upper_bound)
+        self.v = np.random.uniform(-(upper_bound - lower_bound), upper_bound - lower_bound)
 
 
 class ParticleSwarmOptimization:
@@ -36,7 +38,17 @@ class ParticleSwarmOptimization:
     :type upper_bound: numpy array.
     """
     def __init__(self, hyperparams, lower_bound, upper_bound):
-        # Todo: implement
+        self.particles = [
+            Particle(lower_bound, upper_bound)
+            for _ in range(hyperparams.num_particles)
+        ]
+        self.current_particle_index = 0
+        self.best_global_position = np.zeros(np.size(lower_bound))
+        self.best_global_value = -inf
+        self.num_particles = hyperparams.num_particles
+        self.inertia_weight = hyperparams.inertia_weight
+        self.cognitive_parameter = hyperparams.cognitive_parameter
+        self.social_parameter = hyperparams.social_parameter
         self.lower_bound = lower_bound
         self.upper_bound = upper_bound
 
@@ -47,8 +59,7 @@ class ParticleSwarmOptimization:
         :return: the best position.
         :rtype: numpy array.
         """
-        # Todo: implement
-        return self.lower_bound  # Remove this line
+        return self.best_global_position
 
     def get_best_value(self):
         """
@@ -57,8 +68,7 @@ class ParticleSwarmOptimization:
         :return: value of the best position.
         :rtype: float.
         """
-        # Todo: implement
-        return 0.0  # Remove this line
+        return self.best_global_value
 
     def get_position_to_evaluate(self):
         """
@@ -67,15 +77,23 @@ class ParticleSwarmOptimization:
         :return: position to evaluate.
         :rtype: numpy array.
         """
-        # Todo: implement
-        return self.lower_bound  # Remove this line
+        return self.particles[self.current_particle_index].x
 
     def advance_generation(self):
         """
         Advances the generation of particles. Auxiliary method to be used by notify_evaluation().
         """
-        # Todo: implement
-        pass  # Remove this line
+        for i in range(self.num_particles):
+            rp = np.random.uniform(0.0, 1.0)
+            rg = np.random.uniform(0.0, 1.0)
+
+            v_new = (self.inertia_weight * self.particles[i].v +
+                     self.cognitive_parameter * rp * (self.particles[i].best_position - self.particles[i].x) +
+                     self.social_parameter * rg * (self.best_global_position - self.particles[i].x))
+
+            self.particles[i].v = np.clip(v_new, -(self.upper_bound - self.lower_bound), self.upper_bound - self.lower_bound)
+
+            self.particles[i].x = np.clip(self.particles[i].x + self.particles[i].v, self.lower_bound, self.upper_bound)
 
     def notify_evaluation(self, value):
         """
@@ -84,6 +102,14 @@ class ParticleSwarmOptimization:
         :param value: quality of the particle position.
         :type value: float.
         """
-        # Todo: implement
-        pass  # Remove this line
+        if self.particles[self.current_particle_index].best_value < value:
+            self.particles[self.current_particle_index].best_position = self.particles[self.current_particle_index].x
+            self.particles[self.current_particle_index].best_value = value
+        if self.best_global_value < value:
+            self.best_global_position = self.particles[self.current_particle_index].x
+            self.best_global_value = value
 
+        self.current_particle_index = (self.current_particle_index + 1) % self.num_particles
+
+        if self.current_particle_index == 0:
+            self.advance_generation()
